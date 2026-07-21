56900f56012771e3ae8eba51881e774b6b0c0717
khaiwic <khaideptrai13907@gmail.com>
2026-07-21 14:38:42 +0700

    chia cac thu vien can thiet

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,13 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char *id = "TP-Link p301";
+const char *password = "p301p301p301";
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+const char* mqtt_server = "aa9d8788a7654eb88cb3246e1e8b4e6e.s1.eu.hivemq.cloud";
+const int mqtt_port = 8883;
+const char* mqtt_user = "khaideptrai13907@gmail.com";
+const char* mqtt_pass = "Khai@123";
